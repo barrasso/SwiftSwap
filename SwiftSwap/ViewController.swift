c50314097032636b93743e8b90c8036f86aae60a
Mark Barrasso <mark.barrasso@live.com>
2015-01-31 10:38:57 -0500

    Initial Commit

---
+++ SwiftSwap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftSwap
+//
+//  Created by Mark on 1/31/15.
+//  Copyright (c) 2015 MEB. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
